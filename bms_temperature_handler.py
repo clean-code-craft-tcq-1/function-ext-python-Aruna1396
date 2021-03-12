9a6175964ee5324ccc8c3adf3be09c8538c9ced9
Aruna Gopalakrishnan <Gopalakrishnan.Aruna@in.bosch.com>
2021-03-12 11:18:26 +0530

    Remove the usage of Global Variables

--- bms_temperature_handler.py
+++ bms_temperature_handler.py
@@ -1,19 +1,18 @@
 SUPPORTED_TEMPERATURE_UNITS = ['Celsius', 'Fahrenheit', 'Kelvin']
 KELVIN_CONSTANT = 273.15
-charging_temperature_unit = None
 
 
 def set_charging_temperature_unit(temperature_unit):
-    global charging_temperature_unit
     charging_temperature_unit = 'Celsius'
     if temperature_unit not in SUPPORTED_TEMPERATURE_UNITS:
         print('\n ALERT!!! Charging temperature in', temperature_unit, ' is not supported!!!',
               'Proceeding with default Celsius Unit')
     else:
         charging_temperature_unit = temperature_unit
+    return charging_temperature_unit
 
 
-def perform_temperature_conversion_to_celsius(charging_temperature):
+def perform_temperature_conversion_to_celsius(charging_temperature, charging_temperature_unit):
     if charging_temperature_unit == 'Kelvin':
         return charging_temperature - KELVIN_CONSTANT
     else:
@@ -21,8 +20,8 @@ def perform_temperature_conversion_to_celsius(charging_temperature):
 
 
 def perform_temperature_processing(temperature_unit, charging_temperature):
-    set_charging_temperature_unit(temperature_unit)
+    charging_temperature_unit = set_charging_temperature_unit(temperature_unit)
     if charging_temperature_unit != 'Celsius':
-        return perform_temperature_conversion_to_celsius(charging_temperature)
+        return perform_temperature_conversion_to_celsius(charging_temperature,charging_temperature_unit)
     else:
         return charging_temperature
